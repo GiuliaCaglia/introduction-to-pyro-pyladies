386cb79ea246b8ed9e98edac4ed59232eb10c6bf
Marco Caglia <marco.caglia@objectiveplatform.com>
2023-05-28 12:17:51 +0200

    Add script to curl dependencies

---
+++ load_dependencies.sh
@@ -0,0 +1,4 @@
+mkdir assets/
+curl https://raw.githubusercontent.com/GiuliaCaglia/introduction-to-pyro-pyladies/main/requirements.txt > requirements.txt
+curl https://raw.githubusercontent.com/GiuliaCaglia/introduction-to-pyro-pyladies/main/assets/media_prepped.parquet > assets/media_prepped.parquet
+curl https://raw.githubusercontent.com/GiuliaCaglia/introduction-to-pyro-pyladies/main/assets/sales_prepped.parquet > assets/sales_prepped.parquet
